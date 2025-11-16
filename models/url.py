4b8e93c1082b837d81816e72873915f2b53a2423
Muhamed Ads <kg7221996@gmail.com>
2025-11-16 22:13:24 +0200

    Deleting unnecessary files

--- models/url.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from ashredis import RecordBase, MISSING
-from typing import Optional
-
-@dataclass
-class Url(RecordBase):
-    id: str 
-    short_id: Optional[str] = MISSING  # короткий ID как строка
-    original_url: Optional[str] = MISSING  # URL как строка
